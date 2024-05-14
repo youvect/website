e3e51ff659d393818a640c6591bcb4cecd25570b
Robert Love <robertjohnlove@gmail.com>
2024-05-14 18:02:09 +1000

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+---
+layout: null
+---
+{% include_relative custom/jquery.min.js %}
+{% include_relative custom/popper.min.js %}
+{% include_relative bootstrap/bootstrap.min.js %}
+{% include_relative custom/plugins.min.js %}
+{% include_relative custom/custom.min.js %}
